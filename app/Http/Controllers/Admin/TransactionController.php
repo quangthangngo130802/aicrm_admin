f6e0f848a4dc1ac116b9c115ce352a4c29d1d7f8
thangngo.dev <thangngo130802@gmail.com>
2025-03-19 15:27:30 +0700

    a

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -106,7 +106,7 @@ class TransactionController extends Controller
         $bank_id = $superAdmin->bank->shortName;
         $bank_account = $superAdmin->bank_account;
         //Account công ty
-        $bank_company_id = $superAdmin->bankCompany->shortName;
+        $bank_company_id = $superAdmin->bank->shortName;
         $bank_company_account = $superAdmin->bank_company_account;
         $description = $request->input('description');
         $account_name = $superAdmin->name;
